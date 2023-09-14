3fc98241f929c60984fa264dcb180c4c5fd021ac
Lucas Hocker <lucas.hocker@dynatrace.com>
2023-09-14 10:16:02 +0200

    DTHUB-2956 - bump version

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ const config: CliOptions = {
   icon: "src/assets/Au.png",
   app: {
     name: "AutoUpdate Manager",
-    version: "0.0.20",
+    version: "0.0.21",
     description: "A Sample App which enables controlling AutoUpdate at large scale",
     id: "my.auto.update.manager",
     scopes: [
